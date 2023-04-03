3a824378823422e1a99ef549f7a6bf6b1ea5ee0e
DaihiTokuyama <115680629+DaihiTokuyama@users.noreply.github.com>
2023-04-03 20:48:02 +0900

    デス

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,47 +11,82 @@ public class Player : MonoBehaviour
     public LayerMask enemyLayer;
     public AudioClip attackSE;
     public AudioClip deathAttackSE;
-
+    public SpriteRenderer spriteRenderer;
     bool canAttack;
+    bool canDeathAttack;
 
     private void Start() 
     {
-        canAttack = true;    
+        canAttack = true;
+        canDeathAttack = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update() 
     {
+        // サークル範囲内に、レイヤーがEnemyのオブジェクトがいたら取得
+        Collider2D[] hitEnemys = 
+        Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enemyLayer);
+
         if (canAttack == true)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Attack();
-            } 
+                Attack(hitEnemys);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                DeahAttack(hitEnemys);
+            }
+            
+            if (Input.GetKey(KeyCode.E))
+            {
+                Guard();
+            }
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+                ResetColor();
+            }  
         }        
     }
 
-    void Attack()
+    void Attack(Collider2D[] hitEnemys)
     {
-        // サークル範囲内に、レイヤーがEnemyのオブジェクトがいたら取得
-        Collider2D[] hitEnemys = 
-        Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enemyLayer);
-        
         // 取得したそれぞれのEnemyにダメージを与える
         foreach (Collider2D hitEnemy in hitEnemys)
         {
-            var enemy = hitEnemy.GetComponent<Enemy>();   
-            if (enemy.isExhausted == true)
-            {
-                GameManager.instance.PlaySe(deathAttackSE);
-                enemy.Death();
-                canAttack = false;
-            }
-            else if (enemy.isExhausted == false)
+            var enemy = hitEnemy.GetComponent<Enemy>();              
+            if (enemy.isExhausted == false)
             {
                 GameManager.instance.PlaySe(attackSE);
                 enemy.Damage(attackPower);
             }
         }
+    }
+
+    void DeahAttack(Collider2D[] hitEnemys)
+    {
+        foreach (Collider2D hitEnemy in hitEnemys)
+        {
+            var enemy = hitEnemy.GetComponent<Enemy>();              
+            if (enemy.isExhausted == true)
+            {
+                GameManager.instance.PlaySe(deathAttackSE);
+                enemy.Death();
+            }
+        }
+    }
+
+
+    void Guard()
+    {
+        spriteRenderer.color = Color.yellow;
+    }
+
+    void ResetColor()
+    {
+        spriteRenderer.color = Color.blue;
     }
 
     // AttackPointの表示
